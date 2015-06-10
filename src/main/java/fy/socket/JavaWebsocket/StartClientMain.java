d56347d948a68e00af1aad92becf933c5e7b8f4b
wuyinxian124 <wuyinxian124@gmail.com>
2015-06-10 14:16:30 +0800

    修改测试方法，实现多种按照输入测试相关方法

--- src/main/java/fy/socket/JavaWebsocket/StartClientMain.java
+++ src/main/java/fy/socket/JavaWebsocket/StartClientMain.java
@@ -1,9 +1,14 @@
 package fy.socket.JavaWebsocket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
 import java.util.concurrent.TimeUnit;
+
+import javax.swing.InputMap;
 
 import fy.socket.JavaWebsocket.exception.ConnectWebsocketException;
 import fy.socket.JavaWebsocket.exception.HandshakeWebsocketException;
@@ -20,10 +25,24 @@ public class StartClientMain {
 			APPClient client = new APPClient("222.201.139.159", 8877);
 			client.connection();
 			TimeUnit.SECONDS.sleep(5);
-			client.virify("user0", "verify0","homewtb");
+			client.virify("user1", "verify1","homewtb");
 			TimeUnit.SECONDS.sleep(10);
 			//client.sendMsg(null,10,2);
-			client.sendPing(0);
+			BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+			while(true){
+				String command = console.readLine();
+				if(command == null){
+					break;
+				}else if("bye".equals(command)){
+					client.close(0);
+					break;
+				}else if("ping".equals(command)){
+					client.sendPing(0);
+				}else {
+					client.sendMsgText(command,0);
+				}
+			}
+			
 		} catch (ConnectWebsocketException   e) {
 			e.printStackTrace();
 		} catch (URISyntaxException e) {
